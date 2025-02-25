9f4dbaa84800270a4ccf0370bc0579118819193a
Brandon Berhent <bbedward@gmail.com>
2025-02-25 19:26:33 +0000

    Use real IP address in log middleware

---
+++ internal/utils/net_headers.go
@@ -0,0 +1,17 @@
+package utils
+
+import "net/http"
+
+func GetIPAddress(r *http.Request) string {
+	IPAddress := r.Header.Get("CF-Connecting-IP")
+	if IPAddress == "" {
+		IPAddress = r.Header.Get("X-Real-Ip")
+	}
+	if IPAddress == "" {
+		IPAddress = r.Header.Get("X-Forwarded-For")
+	}
+	if IPAddress == "" {
+		IPAddress = r.RemoteAddr
+	}
+	return IPAddress
+}
