579f7d237424ca87d338b28ec733f852e4c4c7d3
Brandon Berhent <bbedward@gmail.com>
2025-03-07 10:51:42 -0500

    Re-add unique constraint to access token

--- cmd/oauth/constants.go
+++
@@ -1,9 +0,0 @@
-package oauth
-
-import (
-	"time"
-)
-
-const ACCESS_TOKEN_EXP = 1 * time.Minute
-const REFRESH_TOKEN_EXP = 24 * time.Hour * 14 // 2 weeks
-var ALLOWED_SCOPES = []string{"openid", "profile", "email", "offline_access"}
--- cmd/oauth/server.go
+++ cmd/oauth/server.go
@@ -24,6 +24,10 @@ import (
 	"github.com/valkey-io/valkey-go"
 )
 
+const ACCESS_TOKEN_EXP = 1 * time.Minute
+const REFRESH_TOKEN_EXP = 24 * time.Hour * 14 // 2 weeks
+var ALLOWED_SCOPES = []string{"openid", "profile", "email", "offline_access"}
+
 // Setup the go-oauth2 server
 func setupOAuthServer(ctx context.Context, cfg *config.Config, valkey valkey.Client) *oauth2server.Oauth2Server {
 	manager := manage.NewDefaultManager()
