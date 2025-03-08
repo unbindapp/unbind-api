f136d57ea377691e20ab335367c9ac8309866457
yekta <ngyekta@gmail.com>
2025-03-08 19:00:07 +0300

    2 minute access token expiration

--- cmd/oauth/server.go
+++ cmd/oauth/server.go
@@ -24,7 +24,7 @@ import (
 	"github.com/valkey-io/valkey-go"
 )
 
-const ACCESS_TOKEN_EXP = 1 * time.Hour
+const ACCESS_TOKEN_EXP = 2 * time.Minute
 const REFRESH_TOKEN_EXP = 24 * time.Hour * 14 // 2 weeks
 var ALLOWED_SCOPES = []string{"openid", "profile", "email", "offline_access"}
 
