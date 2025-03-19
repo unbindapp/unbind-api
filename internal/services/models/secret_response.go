5b997c47690e9a8820778afd929df97cd54c09ef
Brandon Berhent <bbedward@gmail.com>
2025-03-19 18:35:27 -0400

    Fix openapi type for secret response

--- internal/services/models/secret_response.go
+++ internal/services/models/secret_response.go
@@ -1,32 +1,39 @@
 package models
 
-import "encoding/json"
+import (
+	"reflect"
 
-type SecretType int
-
-const (
-	TeamSecret SecretType = iota
-	ProjectSecret
-	EnvironmentSecret
-	ServiceSecret
+	"github.com/danielgtaylor/huma/v2"
 )
 
-func (f SecretType) String() string {
-	switch f {
-	case TeamSecret:
-		return "team"
-	case ProjectSecret:
-		return "project"
-	case EnvironmentSecret:
-		return "environment"
-	case ServiceSecret:
-		return "service"
-	}
-	return "unknown"
+type SecretType string
+
+const (
+	TeamSecret        SecretType = "team"
+	ProjectSecret     SecretType = "project"
+	EnvironmentSecret SecretType = "environment"
+	ServiceSecret     SecretType = "service"
+)
+
+var SecretTypeValues = []SecretType{
+	TeamSecret,
+	ProjectSecret,
+	EnvironmentSecret,
+	ServiceSecret,
 }
 
-func (f SecretType) MarshalJSON() ([]byte, error) {
-	return json.Marshal(f.String())
+// Register enum in OpenAPI specification
+// https://github.com/danielgtaylor/huma/issues/621
+func (u SecretType) Schema(r huma.Registry) *huma.Schema {
+	if r.Map()["SecretType"] == nil {
+		schemaRef := r.Schema(reflect.TypeOf(""), true, "SecretType")
+		schemaRef.Title = "SecretType"
+		for _, v := range SecretTypeValues {
+			schemaRef.Enum = append(schemaRef.Enum, string(v))
+		}
+		r.Map()["SecretType"] = schemaRef
+	}
+	return &huma.Schema{Ref: "#/components/schemas/SecretType"}
 }
 
 type SecretResponse struct {
