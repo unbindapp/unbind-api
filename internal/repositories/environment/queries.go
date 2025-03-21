2d21efde30f12f963c70111dd213240b45b36c68
yekta <ngyekta@gmail.com>
2025-03-22 02:39:06 +0300

    Include project in env query

--- internal/repositories/environment/queries.go
+++ internal/repositories/environment/queries.go
@@ -5,8 +5,9 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/unbindapp/unbind-api/ent"
+	"github.com/unbindapp/unbind-api/ent/environment"
 )
 
 func (self *EnvironmentRepository) GetByID(ctx context.Context, id uuid.UUID) (*ent.Environment, error) {
-	return self.base.DB.Environment.Get(ctx, id)
+	return self.base.DB.Environment.Query().Where(environment.ID(id)).WithProject().Only(ctx)
 }
