ce4d9b56c0ab5e6f7d57f5e1d251f9e61274d066
Brandon Berhent <bbedward@gmail.com>
2025-04-07 12:04:31 -0400

    json tags

--- pkg/templates/schema.go
+++ pkg/templates/schema.go
@@ -2,13 +2,12 @@ package templates
 
 // TemplateMetadata represents the metadata of a template
 type TemplateMetadata struct {
-	Name        string                  `yaml:"name"`
-	Description string                  `yaml:"description"`
-	Type        string                  `yaml:"type"`
-	Version     string                  `yaml:"version"`
-	Maintainer  string                  `yaml:"maintainer"`
-	Imports     []TemplateImport        `yaml:"imports,omitempty"`
-	Schema      TemplateParameterSchema `yaml:"schema"`
+	Name        string                  `yaml:"name" json:"name"`
+	Description string                  `yaml:"description" json:"description"`
+	Type        string                  `yaml:"type" json:"type"`
+	Version     string                  `yaml:"version" json:"version"`
+	Imports     []TemplateImport        `yaml:"imports,omitempty" json:"imports,omitempty"`
+	Schema      TemplateParameterSchema `yaml:"schema" json:"schema"`
 }
 
 // TemplateImport represents an import of an external schema
@@ -19,21 +18,21 @@ type TemplateImport struct {
 
 // TemplateParameterSchema defines the structure for allowed parameters
 type TemplateParameterSchema struct {
-	Properties map[string]ParameterProperty `yaml:"properties"`
-	Required   []string                     `yaml:"required,omitempty"`
+	Properties map[string]ParameterProperty `yaml:"properties" json:"properties"`
+	Required   []string                     `yaml:"required,omitempty" json:"required,omitempty"`
 	Imports    map[string]interface{}       `json:"-" yaml:"-"` // For resolved imports
 }
 
 // ParameterProperty defines a single parameter's schema
 type ParameterProperty struct {
-	Type        string                       `yaml:"type"`
-	Description string                       `yaml:"description,omitempty"`
-	Default     interface{}                  `yaml:"default,omitempty"`
-	Enum        []string                     `yaml:"enum,omitempty"`
-	Properties  map[string]ParameterProperty `yaml:"properties,omitempty"`
-	Ref         string                       `yaml:"$ref,omitempty"`
-	Minimum     *float64                     `yaml:"minimum,omitempty"`
-	Maximum     *float64                     `yaml:"maximum,omitempty"`
+	Type        string                       `yaml:"type" json:"type"`
+	Description string                       `yaml:"description,omitempty" json:"description,omitempty"`
+	Default     interface{}                  `yaml:"default,omitempty" json:"default,omitempty"`
+	Enum        []string                     `yaml:"enum,omitempty" json:"enum,omitempty"`
+	Properties  map[string]ParameterProperty `yaml:"properties,omitempty" json:"properties,omitempty"`
+	Ref         string                       `yaml:"$ref,omitempty" json:"$ref,omitempty"`
+	Minimum     *float64                     `yaml:"minimum,omitempty" json:"minimum,omitempty"`
+	Maximum     *float64                     `yaml:"maximum,omitempty" json:"maximum,omitempty"`
 }
 
 // Template represents a fully resolved template
