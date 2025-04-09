bfd2dd9f4c6938f69328704109edefe08934e5bb
Brandon Berhent <bbedward@gmail.com>
2025-04-09 08:05:31 -0400

    include version

--- internal/services/models/service_config_response.go
+++ internal/services/models/service_config_response.go
@@ -8,17 +8,18 @@ import (
 
 // ServiceConfigResponse defines the configuration response for a service
 type ServiceConfigResponse struct {
-	GitBranch  *string               `json:"git_branch,omitempty"`
-	Type       schema.ServiceType    `json:"type"`
-	Builder    schema.ServiceBuilder `json:"builder"`
-	Icon       string                `json:"icon"`
-	Host       []v1.HostSpec         `json:"hosts,omitempty" nullable:"false"`
-	Port       []v1.PortSpec         `json:"ports,omitempty" nullable:"false"`
-	Replicas   int32                 `json:"replicas"`
-	AutoDeploy bool                  `json:"auto_deploy"`
-	RunCommand *string               `json:"run_command,omitempty"`
-	Public     bool                  `json:"public"`
-	Image      string                `json:"image,omitempty"`
+	GitBranch       *string               `json:"git_branch,omitempty"`
+	Type            schema.ServiceType    `json:"type"`
+	Builder         schema.ServiceBuilder `json:"builder"`
+	Icon            string                `json:"icon"`
+	Host            []v1.HostSpec         `json:"hosts,omitempty" nullable:"false"`
+	Port            []v1.PortSpec         `json:"ports,omitempty" nullable:"false"`
+	Replicas        int32                 `json:"replicas"`
+	AutoDeploy      bool                  `json:"auto_deploy"`
+	RunCommand      *string               `json:"run_command,omitempty"`
+	Public          bool                  `json:"public"`
+	Image           string                `json:"image,omitempty"`
+	DatabaseVersion *string               `json:"database_version,omitempty"`
 }
 
 // TransformServiceConfigEntity transforms an ent.ServiceConfig entity into a ServiceConfigResponse
@@ -26,17 +27,18 @@ func TransformServiceConfigEntity(entity *ent.ServiceConfig) *ServiceConfigRespo
 	response := &ServiceConfigResponse{}
 	if entity != nil {
 		response = &ServiceConfigResponse{
-			GitBranch:  entity.GitBranch,
-			Type:       entity.Type,
-			Builder:    entity.Builder,
-			Icon:       entity.Icon,
-			Host:       entity.Hosts,
-			Port:       entity.Ports,
-			Replicas:   entity.Replicas,
-			AutoDeploy: entity.AutoDeploy,
-			RunCommand: entity.RunCommand,
-			Public:     entity.Public,
-			Image:      entity.Image,
+			GitBranch:       entity.GitBranch,
+			Type:            entity.Type,
+			Builder:         entity.Builder,
+			Icon:            entity.Icon,
+			Host:            entity.Hosts,
+			Port:            entity.Ports,
+			Replicas:        entity.Replicas,
+			AutoDeploy:      entity.AutoDeploy,
+			RunCommand:      entity.RunCommand,
+			Public:          entity.Public,
+			Image:           entity.Image,
+			DatabaseVersion: entity.DatabaseVersion,
 		}
 	}
 	return response
