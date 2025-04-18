d622e0a7a8ff17ea8155d56ebe2170a1497c9b35
Brandon Berhent <bbedward@gmail.com>
2025-04-18 15:02:23 -0400

    Fix validation

--- ent/schema/registry.go
+++ ent/schema/registry.go
@@ -3,6 +3,8 @@ package schema
 import (
 	"errors"
 	"net/url"
+	"strconv"
+	"strings"
 
 	"entgo.io/ent"
 	"entgo.io/ent/dialect/entsql"
@@ -28,19 +30,39 @@ func (Registry) Mixin() []ent.Mixin {
 func (Registry) Fields() []ent.Field {
 	return []ent.Field{
 		field.String("host").Validate(func(s string) error {
-			// Should be valid host without protocol
-			// e.g. registry.example.com , registry.example.com:5000
-			// but not https://registry.example.com
-			parsedURL, err := url.Parse(s)
+			if strings.Count(s, ":") == 1 {
+				parts := strings.Split(s, ":")
+				hostname := parts[0]
+				port := parts[1]
+
+				// Validate hostname
+				if hostname == "" {
+					return errors.New("host should not be empty")
+				}
+
+				// Validate port
+				if _, err := strconv.Atoi(port); err != nil {
+					return errors.New("invalid port number")
+				}
+
+				return nil
+			}
+
+			// Try normla parsing and add a schema
+			parsedURL, err := url.Parse("http://" + s)
 			if err != nil {
 				return errors.New("invalid host format")
 			}
-			if parsedURL.Scheme != "" {
-				return errors.New("host should not contain protocol")
-			}
+
 			if parsedURL.Hostname() == "" {
 				return errors.New("host should not be empty")
 			}
+
+			// Make sure the original input doesn't have a scheme
+			if strings.Contains(s, "://") {
+				return errors.New("host should not contain protocol")
+			}
+
 			return nil
 		}),
 		field.String("kubernetes_secret").Optional().Nillable().
