b2b19886bfef3606aecd5e233200753bc532c558
Brandon Berhent <bbedward@gmail.com>
2025-04-25 10:46:05 -0400

    Fix query

--- internal/infrastructure/prometheus/metrics_query.go
+++ internal/infrastructure/prometheus/metrics_query.go
@@ -56,11 +56,9 @@ func (self *PrometheusClient) GetResourceMetrics(
 
 	diskQuery := fmt.Sprintf(`sum by (%s) (
 		kubelet_volume_stats_used_bytes
-			* on (namespace, persistentvolumeclaim)
-				group_left(pod)
-				kube_pod_spec_volumes_persistentvolumeclaim_info
-			* on (namespace, pod)
-				group_left(label_unbind_team,label_unbind_project,label_unbind_environment,label_unbind_service)
+			* on(namespace,persistentvolumeclaim) group_left(pod)
+				kube_pod_spec_volumes_persistentvolumeclaims_info
+			* on(namespace,pod) group_left(label_unbind_service)
 				kube_pod_labels%s
 	)`, sumBy.Label(), kubeLabelsSelector)
 
