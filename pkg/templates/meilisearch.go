f89770ac48cf97f20b28ecaa86663e79a4a6b44b
Brandon Berhent <bbedward@gmail.com>
2025-05-10 13:40:20 -0400

    Add meili template

---
+++ pkg/templates/meilisearch.go
@@ -0,0 +1,75 @@
+package templates
+
+import (
+	"github.com/unbindapp/unbind-api/ent/schema"
+	"github.com/unbindapp/unbind-api/internal/common/utils"
+)
+
+// MeiliSearchTemplate returns the predefined MeiliSearch template
+func meiliSearchTemplate() *schema.TemplateDefinition {
+	return &schema.TemplateDefinition{
+		Name:        "meilisearch",
+		Version:     1,
+		Description: "MeiliSearch - A powerful, fast, open-source, easy to use and deploy search engine",
+		Inputs: []schema.TemplateInput{
+			{
+				ID:          1,
+				Name:        "API Host",
+				Type:        schema.InputTypeHost,
+				Description: "Hostname to use for the MeiliSearch API server.",
+				Required:    true,
+				TargetPort:  utils.ToPtr(7700),
+			},
+			{
+				ID:          2,
+				Name:        "Storage Size",
+				Type:        schema.InputTypeVolumeSize,
+				Description: "Size of the persistent storage for MeiliSearch data.",
+				Required:    true,
+				Default:     utils.ToPtr("1Gi"),
+			},
+		},
+		Services: []schema.TemplateService{
+			{
+				ID:           1,
+				Name:         "MeiliSearch",
+				Type:         schema.ServiceTypeDockerimage,
+				Builder:      schema.ServiceBuilderDocker,
+				HostInputIDs: []int{1},
+				Image:        utils.ToPtr("getmeili/meilisearch:v1.14"),
+				Ports: []schema.PortSpec{
+					{
+						Port:     7700,
+						Protocol: utils.ToPtr(schema.ProtocolTCP),
+					},
+				},
+				IsPublic: true,
+				Variables: []schema.TemplateVariable{
+					{
+						Name: "MEILI_MASTER_KEY",
+						Generator: &schema.ValueGenerator{
+							Type: schema.GeneratorTypePassword,
+						},
+					},
+					{
+						Name:  "MEILI_HTTP_ADDR",
+						Value: "0.0.0.0:7700",
+					},
+					{
+						Name:  "MEILI_ENV",
+						Value: "production",
+					},
+				},
+				Volumes: []schema.TemplateVolume{
+					{
+						Name: "meilisearch-data",
+						Size: schema.TemplateVolumeSize{
+							FromInputID: 2,
+						},
+						MountPath: "/meili_data",
+					},
+				},
+			},
+		},
+	}
+}
--- pkg/templates/templater.go
+++ pkg/templates/templater.go
@@ -19,5 +19,6 @@ func (self *Templater) AvailableTemplates() []*schema.TemplateDefinition {
 		wordPressTemplate(),
 		ghostTemplate(),
 		minioTemplate(),
+		meiliSearchTemplate(),
 	}
 }
