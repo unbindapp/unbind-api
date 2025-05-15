7672826677ee7694fccaafd4becb0d3d48e2a57b
Brandon Berhent <bbedward@gmail.com>
2025-05-15 14:25:46 -0400

    Update strapi

--- pkg/templates/strapi.go
+++ pkg/templates/strapi.go
@@ -24,7 +24,7 @@ func strapiTemplate() *schema.TemplateDefinition {
 			},
 			{
 				ID:   2,
-				Name: "Storage Size",
+				Name: "Upload Storage Size",
 				Type: schema.InputTypeVolumeSize,
 				Volume: &schema.TemplateVolume{
 					Name:      "strapi-upload-data",
@@ -33,6 +33,30 @@ func strapiTemplate() *schema.TemplateDefinition {
 				Description: "Size of the persistent storage for Strapi uploads.",
 				Required:    true,
 				Default:     utils.ToPtr("512Mi"),
+			},
+			{
+				ID:   3,
+				Name: "Source Code Storage Size",
+				Type: schema.InputTypeVolumeSize,
+				Volume: &schema.TemplateVolume{
+					Name:      "strapi-src",
+					MountPath: "/opt/app/src",
+				},
+				Description: "Persistent volume for Strapi source code (e.g. APIs, components).",
+				Required:    true,
+				Default:     utils.ToPtr("512Mi"),
+			},
+			{
+				ID:   4,
+				Name: "Config Storage Size",
+				Type: schema.InputTypeVolumeSize,
+				Volume: &schema.TemplateVolume{
+					Name:      "strapi-config",
+					MountPath: "/opt/app/config",
+				},
+				Description: "Persistent volume for Strapi configuration files.",
+				Required:    true,
+				Default:     utils.ToPtr("256Mi"),
 			},
 			{
 				ID:          3,
@@ -47,7 +71,7 @@ func strapiTemplate() *schema.TemplateDefinition {
 			{
 				ID:           1,
 				Name:         "PostgreSQL",
-				InputIDs:     []int{3},
+				InputIDs:     []int{5},
 				Type:         schema.ServiceTypeDatabase,
 				Builder:      schema.ServiceBuilderDatabase,
 				DatabaseType: utils.ToPtr("postgres"),
@@ -55,18 +79,37 @@ func strapiTemplate() *schema.TemplateDefinition {
 			{
 				ID:        2,
 				DependsOn: []int{1},
-				InputIDs:  []int{1, 2},
+				InputIDs:  []int{1, 2, 3, 4},
 				Name:      "Strapi",
 				Type:      schema.ServiceTypeDockerimage,
 				Builder:   schema.ServiceBuilderDocker,
-				Image:     utils.ToPtr("elestio/strapi-development:v5.12.6"),
+				Image:     utils.ToPtr("ghcr.io/unbindapp/strapi-development:v5.12.6"),
 				Ports: []schema.PortSpec{
 					{
 						Port:     1337,
 						Protocol: utils.ToPtr(schema.ProtocolTCP),
 					},
 				},
+				HealthCheck: &schema.HealthCheck{
+					Type: schema.HealthCheckTypeHTTP,
+					Path: "/",
+					Port: utils.ToPtr(int32(1337)),
+					// High trhesholds because it does some bootstrap stuff
+					PeriodSeconds:             10,
+					TimeoutSeconds:            5,
+					StartupFailureThreshold:   10,
+					LivenessFailureThreshold:  3,
+					ReadinessFailureThreshold: 10,
+				},
 				Variables: []schema.TemplateVariable{
+					{
+						Name: "BASE_URL",
+						Generator: &schema.ValueGenerator{
+							Type:      schema.GeneratorTypeInput,
+							InputID:   1,
+							AddPrefix: "https://",
+						},
+					},
 					{
 						Name:  "DATABASE_CLIENT",
 						Value: "postgres",
@@ -86,6 +129,10 @@ func strapiTemplate() *schema.TemplateDefinition {
 					{
 						Name:  "STRAPI_PLUGIN_I18N_INIT_LOCALE_CODE",
 						Value: "en",
+					},
+					{
+						Name:  "STRAPI_ENFORCE_SOURCEMAPS",
+						Value: "false",
 					},
 					{
 						Name: "JWT_SECRET",
