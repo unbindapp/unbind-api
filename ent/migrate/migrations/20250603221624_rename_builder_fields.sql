c41447d5f84c29f4243e1a552698bf8f4395991b
yekta <ngyekta@gmail.com>
2025-06-04 11:17:56 +0300

    Fix the migration

--- ent/migrate/migrations/20250603221624_rename_builder_fields.sql
+++ ent/migrate/migrations/20250603221624_rename_builder_fields.sql
@@ -1,15 +1,34 @@
 -- +goose Up
 -- modify "service_configs" table
-ALTER TABLE "service_configs" 
-  RENAME COLUMN "docker_builder_path" TO "docker_builder_dockerfile_path",
-  RENAME COLUMN "docker_builder_context" TO "docker_builder_build_context",
-  RENAME COLUMN "install_command" TO "railpack_builder_install_command",
-  RENAME COLUMN "build_command" TO "railpack_builder_build_command";
+ALTER TABLE "service_configs"
+RENAME COLUMN "docker_builder_path" TO "docker_builder_dockerfile_path";
+
+
+ALTER TABLE "service_configs"
+RENAME COLUMN "docker_builder_context" TO "docker_builder_build_context";
+
+
+ALTER TABLE "service_configs"
+RENAME COLUMN "install_command" TO "railpack_builder_install_command";
+
+
+ALTER TABLE "service_configs"
+RENAME COLUMN "build_command" TO "railpack_builder_build_command";
+
 
 -- +goose Down
 -- reverse: modify "service_configs" table
-ALTER TABLE "service_configs" 
-  RENAME COLUMN "railpack_builder_build_command" TO "build_command",
-  RENAME COLUMN "railpack_builder_install_command" TO "install_command",
-  RENAME COLUMN "docker_builder_build_context" TO "docker_builder_context",
-  RENAME COLUMN "docker_builder_dockerfile_path" TO "docker_builder_path";
+ALTER TABLE "service_configs"
+RENAME COLUMN "railpack_builder_build_command" TO "build_command";
+
+
+ALTER TABLE "service_configs"
+RENAME COLUMN "railpack_builder_install_command" TO "install_command";
+
+
+ALTER TABLE "service_configs"
+RENAME COLUMN "docker_builder_build_context" TO "docker_builder_context";
+
+
+ALTER TABLE "service_configs"
+RENAME COLUMN "docker_builder_dockerfile_path" TO "docker_builder_path";
